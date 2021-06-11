88987a43d104d804d7813ea950f2722da72f20e0
Rendy Sulu <idforgithub@users.noreply.github.com>
2021-06-11 19:54:48 +0700

    Update canvas.js

--- assets/canvas.js
+++ assets/canvas.js
@@ -27,15 +27,16 @@ class Circle{
         y: undefined
     }
 
-    //speedX = undefined
-    //speedY = undefined
-
     tick = 1
     mass = 1
+    opacity = 0
 
     constructor(x = 0, y = 0, radius = 0, color = 'black'){
-        this.x = x
-        this.y = y
+        this.vector = {
+            x: x,
+            y: y
+        }
+
         this.radius = radius
         this.color = color
 
@@ -44,67 +45,65 @@ class Circle{
     }
 
     #fixPosition(){
-        this.x = (this.x + this.radius) > innerWidth ? (this.x - this.radius) : this.x
-        this.x = (this.x - this.radius) < 0 ? (this.x + this.radius) : this.x
+        this.vector.x = (this.vector.x + this.radius) > innerWidth ? (this.vector.x - this.radius) : this.vector.x
+        this.vector.x = (this.vector.x - this.radius) < 0 ? (this.vector.x + this.radius) : this.vector.x
         
-        this.y = (this.y + this.radius > innerHeight) ? (this.y - this.radius) : this.y
-        this.y = (this.y - this.radius) < 0 ? (this.y + this.radius) : this.y
+        this.vector.y = (this.vector.y + this.radius > innerHeight) ? (this.vector.y - this.radius) : this.vector.y
+        this.vector.y = (this.vector.y - this.radius) < 0 ? (this.vector.y + this.radius) : this.vector.y
     }
 
     randomSpeed(isRandom = true){
-        this.velocity.x = (isRandom ? Math.random().toFixed(1) : 1) * (Math.random() < 0.5 ? this.tick : -this.tick)
-        this.velocity.y = (isRandom ? Math.random().toFixed(1) : 1) * (Math.random() < 0.5 ? this.tick : -this.tick)
-    }
-
-    number = undefined
-    setNumber(setIndex = 0){
-        this.number = setIndex
+        this.velocity = {
+            x: (isRandom ? Math.random().toFixed(1) : 1) * (Math.random() < 0.9 ? this.tick : -this.tick),
+            y: (isRandom ? Math.random().toFixed(1) : 1) * (Math.random() < 0.9 ? this.tick : -this.tick)
+        }
     }
 
     draw(){
         context.beginPath()
-        context.arc(this.x, this.y, this.radius, 0, Math.pow(Math.PI, 2))
+        context.arc(this.vector.x, this.vector.y, this.radius, 0, Math.pow(Math.PI, 2))
+        context.save()
+        context.globalAlpha = this.opacity
+        context.fillStyle = this.color
+        context.fill()
+        context.restore()
         context.strokeStyle = this.color
-
-        if(undefined != this.number){
-            context.font = "10px Arial"
-            context.fillText(""+this.number, this.x, this.y)
-        }
-
         context.stroke()
         context.closePath()
     }
 
-    circleArray = []
-    setCollide(circles){
-        this.circleArray = circles
-    }
-
-    collideUpdate(){
-        if(this.circleArray.length != 0){
-            this.circleArray.forEach(circleFromArray => {
-                if(this !== circleFromArray){
-                    const distance = distanceBetweenCircle(this, circleFromArray)
-                    if(distance < 0){
-                        //console.log("collide")
-                        resolveCollision(this, circleFromArray)
-                    }
+    collideUpdate(circles){
+        circles.forEach(circleFromArray => {
+            if(this !== circleFromArray){
+                const distance = distanceBetweenCircle(this, circleFromArray)
+                if(distance <= 0){
+                    resolveCollision(this, circleFromArray)
+                    this.color = requestColor()
+                    circleFromArray.color = requestColor()
                 }
-                
-            })
-        }
+            }
+        })
         this.update()
     }
 
     update(){
-        if( this.x + this.radius > innerWidth || this.x - this.radius < 0)
+        if( this.vector.x + this.radius > innerWidth || this.vector.x - this.radius < 0)
             this.velocity.x = -this.velocity.x
         
-        if( this.y + this.radius > innerHeight || this.y - this.radius < 0)
+        if( this.vector.y + this.radius > innerHeight || this.vector.y - this.radius < 0)
             this.velocity.y = -this.velocity.y
         
-        this.x += this.velocity.x
-        this.y += this.velocity.y
+        
+        if(calculateDistance(mouseVector.x, mouseVector.y, this.vector.x, this.vector.y) < mouseInteractRadius){
+            this.opacity += .1
+            this.opacity = Math.min(this.opacity, 1)
+        } else {
+            this.opacity -= .1
+            this.opacity = Math.max(this.opacity, 0)
+        }
+
+        this.vector.x += this.velocity.x
+        this.vector.y += this.velocity.y
 
         this.draw()
     }
@@ -112,44 +111,43 @@ class Circle{
 
 function rotate(velocity = {x: undefined, y: undefined}, angle = undefined){
     return {
-        x: (velocity.x * Math.cos(angle)) - (velocity.y * Math.sin(angle)),
-        y: (velocity.x * Math.sin(angle)) - (velocity.y * Math.cos(angle)),
+        x: velocity.x * Math.cos(angle) + velocity.y * Math.sin(angle),
+        y: -velocity.x * Math.sin(angle) + velocity.y * Math.cos(angle)
     }
 }
 
 function resolveCollision(circle1, circle2){
-    const xVelocityDiff = circle1.velocity.x - circle2.velocity.x
-    const yVelocityDiff = circle1.velocity.y - circle2.velocity.y
+    const velocityDiff = {
+        x: circle1.velocity.x - circle2.velocity.x,
+        y: circle1.velocity.y - circle2.velocity.y
+    }
+    const distance = {
+        x: circle2.vector.x - circle1.vector.x,
+        y: circle2.vector.y - circle1.vector.y
+    }
 
-    const xDist = circle1.x - circle2.x
-    const yDist = circle1.y - circle2.y
-
-    if( xVelocityDiff * xDist + yVelocityDiff * yDist >= 0){
-        const angle = -Math.atan2(circle2.y - circle1.y, circle2.x - circle1.x)
+    if((velocityDiff.x * distance.x) + (velocityDiff.y * distance.y) >= 0){
+        const angle = Math.atan2(circle2.vector.y - circle1.vector.y, circle2.vector.x - circle1.vector.x)
 
         const m1 = circle1.mass
         const m2 = circle2.mass
 
-        const u1 = this.rotate(circle1.velocity, angle)
-        const u2 = this.rotate(circle2.velocity, angle)
-
+        const u1 = rotate(circle1.velocity, angle)
+        const u2 = rotate(circle2.velocity, angle)
+        
         const v1 = { 
-            x: (u1.x * (m1 - m2) + ((m2 * 2) * u2.x)) / (m1 + m2),
+            x: (u1.x * (m1 - m2) + ((m2 * 2) * u2.x)) / 
+                         (m1 + m2),
             y: u1.y
         }
         const v2 = {
-            x: (u2.x * (m2 - m1) + ((m1 * 2) * u1.x)) / (m1 + m2),
+            x: (u2.x * (m2 - m1) + ((m1 * 2) * u1.x)) / 
+                         (m1 + m2),
             y: u2.y
         }
 
-        const finalVelolicy1 = rotate(v1, -angle)
-        const finalVelolicy2 = rotate(v2, -angle)
-
-        circle1.velocity.x = finalVelolicy1.x
-        circle1.velocity.y = finalVelolicy1.y
-
-        circle2.velocity.x = finalVelolicy2.x
-        circle2.velocity.y = finalVelolicy2.y
+        circle1.velocity = rotate(v1, -angle)
+        circle2.velocity = rotate(v2, -angle)
     }
 }
 
@@ -163,53 +161,47 @@ function randomNumber(number1, number2, rounding = false) {
         Math.random() * (number2 - number1) + number1
 }
 
+function requestColor(){
+    return strColorRGBA(
+        randomNumber(0, 255, true),
+        randomNumber(0, 255, true),
+        randomNumber(0, 255, true),
+        randomNumber(0.2, 1.0),
+    )
+}
+
+var rMinMax = {
+    min: undefined,
+    max: undefined
+}
 const circles = []
+
+function requestRadius(min = 10, max = 100) {
+    rMinMax = {
+        min: min,
+        max: max
+    }
+}
 
 function instantiate(target = 0, index = 0) {
     if(index < target){
-        let rngRadius = randomNumber(20, 50)
-        let circle = new Circle(
-            randomNumber(rngRadius, innerWidth - rngRadius), 
-            randomNumber(rngRadius, innerHeight - rngRadius),
-            rngRadius, 
-            "black"/*
-            strColorRGBA(
-                randomNumber(0, 255, true),
-                randomNumber(0, 255, true),
-                randomNumber(0, 255, true),
-                randomNumber(0.2, 1.0),
-            )
-            */
+        const radius = randomNumber(rMinMax.min, rMinMax.max)
+        const circle = new Circle(
+            randomNumber(radius, innerWidth - radius),  // X
+            randomNumber(radius, innerHeight - radius), // Y
+            radius,                                     // R (radius)
+            requestColor()                              // Color
         )
-        circle.tick = randomNumber(5, 10)
+        circle.tick = randomNumber(2, 10)
         circle.randomSpeed()
-        circle.setNumber(index)
-
-        circles.push(checkElement(circle))
-        circle.setCollide(circles)
+        circles.push(circle)
         
         return instantiate(target, index+1)
     }
 }
 
-// if using recursive it will reach maximum callback!!!
-function checkElement(circle) {
-    if(circles.length == 0)
-        return circle
-    
-    circles.forEach(circleFromArray => {
-        const distance = distanceBetweenCircle(circle, circleFromArray)
-        if(distance < 0){
-            circle.x = randomNumber(circle.radius, innerWidth - circle.radius, true)
-            circle.y = randomNumber(circle.radius, innerHeight - circle.radius, true)
-            return checkElement(circle)
-        }
-    })
-    return circle
-}
-
 function distanceBetweenCircle(circle1, circle2) {
-    return calculateDistance(circle1.x, circle1.y, circle2.x, circle2.y) - Math.hypot(circle1.radius, circle2.radius) * 2
+    return calculateDistance(circle1.vector.x, circle1.vector.y, circle2.vector.x, circle2.vector.y) - (circle1.radius + circle2.radius)
 }
 
 function calculateDistance(x1 = 0, y1 = 0, x2 = 0, y2 = 0){
@@ -221,11 +213,19 @@ function animation(){
     context.clearRect(0, 0, canvas.width, canvas.height)
 
     circles.forEach(c => {
-        c.collideUpdate()
+        c.collideUpdate(circles)
     })
+    
 }
 
+// set interactive between mouse and the circles
+mouseInteractRadius = 100
+
+// set radius on circle, by default min = 10 and max = 100
+requestRadius(min = 5, max = 8)
+
 // target = (how much circle to instantiate)
-instantiate(target = 5)
+instantiate(target = 500)
+
 // run animation
 animation()
